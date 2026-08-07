01988185eea0db05eaf93be3f77f95fce854a824
haniya akhtar <thehaniyaakhtar@gmail.com>
2026-08-07 22:21:09 +0530

    Visualizing removal of noise

--- app/visualizer.py
+++ app/visualizer.py
@@ -47,3 +47,43 @@ def save_spectrogram(audio, sr, output_path):
     plt.savefig(output_path)
 
     plt.close()
+    
+def save_difference_spectrogram(
+    original_audio,
+    cleaned_audio,
+    sr,
+    output_path
+):
+
+    original_spec = np.abs(
+        librosa.stft(original_audio)
+    )
+
+    cleaned_spec = np.abs(
+        librosa.stft(cleaned_audio)
+    )
+
+    difference = (
+        original_spec - cleaned_spec
+    )
+
+    plt.figure(figsize=(12,4))
+
+    librosa.display.specshow(
+        difference,
+        sr=sr,
+        x_axis="time",
+        y_axis="hz"
+    )
+
+    plt.colorbar()
+
+    plt.title(
+        "Removed Noise"
+    )
+
+    plt.tight_layout()
+
+    plt.savefig(output_path)
+
+    plt.close()
