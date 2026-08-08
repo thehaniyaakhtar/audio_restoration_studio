00a4b8c786ee0138ad84a66adf888cf237a99f98
haniya akhtar <thehaniyaakhtar@gmail.com>
2026-08-08 16:41:13 +0530

    Updating Flask App

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-import shutil
 from fastapi.responses import HTMLResponse
+
+import shutil
 import os
 
 from app.pipeline import process_audio
 
+
 app = FastAPI(
     title="AI Audio Restoration Studio"
 )
+
+# Create folders if missing
+os.makedirs("sample_audio", exist_ok=True)
+os.makedirs("outputs", exist_ok=True)
 
 templates = Jinja2Templates(
     directory="templates"
@@ -27,6 +33,7 @@ app.mount(
     name="outputs"
 )
 
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
 
@@ -35,12 +42,19 @@ async def home(request: Request):
         name="index.html"
     )
 
+
 @app.post("/process")
 async def process(file: UploadFile = File(...)):
 
-    input_path = f"sample_audio/{file.filename}"
+    input_path = (
+        f"sample_audio/{file.filename}"
+    )
 
-    with open(input_path, "wb") as buffer:
+    with open(
+        input_path,
+        "wb"
+    ) as buffer:
+
         shutil.copyfileobj(
             file.file,
             buffer
