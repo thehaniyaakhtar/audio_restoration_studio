3449105caab7f2c62d09bb74e5d2b11010831d3b
haniya akhtar <thehaniyaakhtar@gmail.com>
2026-08-09 14:27:13 +0530

    Fixing model runtime bug 2

--- app/transcription.py
+++ app/transcription.py
@@ -1,11 +1,11 @@
 import whisper
 
-model = whisper.load_model("tiny")
-
 def transcribe_audio(audio_path):
-    
+
+    model = whisper.load_model("tiny")
+
     result = model.transcribe(
         audio_path
     )
-    
+
     return result["text"]
